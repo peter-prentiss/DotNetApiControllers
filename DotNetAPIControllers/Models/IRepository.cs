ccd1ffe8e83768eccaa4f9f49c0b5e18446bae59
Peter <peter.prentiss@gmail.com>
2018-04-08 13:41:08 -0500

    Complete IRepository class

--- DotNetAPIControllers/Models/IRepository.cs
+++ DotNetAPIControllers/Models/IRepository.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 namespace DotNetAPIControllers.Models
 {
     public class IRepository
     {
-        public IRepository()
-        {
-        }
+        IEnumerable<Reservation> Reservations { get; }
+        Reservation this[int id] { get; }
+
+        Reservation AddReservation(Reservation reservation);
+        Reservation UpdateReservation(Reservation reservation);
+        void DeleteReservation(int id);
     }
 }
