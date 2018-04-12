0cff8fb16a96a9526691a8100f8e2e33fd157b09
Peter <peter.prentiss@gmail.com>
2018-04-12 14:23:23 -0500

    Create and complete Index view

--- DotNetAPIControllers/Controllers/HomeController.cs
+++ DotNetAPIControllers/Controllers/HomeController.cs
@@ -3,19 +3,22 @@ using DotNetAPIControllers.Models;
 
 namespace DotNetAPIControllers.Controllers
 {
-    public class HomeController : Controller 
+    public class HomeController : Controller
     {
         private IRepository repository { get; set; }
 
-        public HomeController(IRepository repo) {
+        public HomeController(IRepository repo)
+        {
             repository = repo;
         }
 
         public ViewResult Index() => View(repository.Reservations);
 
         [HttpPost]
-        public IActionResult AddReservation(Reservation reservation) {
+        public IActionResult AddReservation(Reservation reservation)
+        {
             repository.AddReservation(reservation);
             return RedirectToAction("Index");
         }
+    }
 }
