2dfbb2c89f0267bf61037ce95b03af37c4097f64
Peter <peter.prentiss@gmail.com>
2018-04-17 13:41:34 -0500

    Update Startup.cs

---
+++ DotNetAPIControllers/Models/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using DotNetAPIControllers.Models;
+
+namespace DotNetAPIControllers {
+    public class Startup {
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddSingleton<IRepository MemorRepository>;
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+
+        }
+    }
+}
