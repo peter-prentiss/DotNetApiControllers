964a0aa8478eb6d095aa5e69687bfbca243ec183
Peter <peter.prentiss@gmail.com>
2018-04-20 14:08:52 -0500

    Complete ReservationClass

--- DotNetAPIControllers/Controllers/ReservationController.cs
+++ DotNetAPIControllers/Controllers/ReservationController.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using DotNetAPIControllers.Models;
+
 namespace DotNetAPIControllers.Controllers
 {
-    public class ReservationController
+    [Route("api/[controller")]
+    public class ReservationController : Controller
     {
-        public ReservationController()
+        private IRepository repository;
+
+        public ReservationController(IRepository repo)
         {
+            repository = repo;
         }
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => repository.Reservations;
+
+        [HttpGet("{id}")]
+        public Reservation Get(int id) => repository[id];
+
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) =>
+            repository.AddReservation(new Reservation
+            {
+                ClientName = res.ClientName;
+                Location = res.Location;
+        });
+
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res) =>
+            repository.UpdateReservation(res);
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repository.DeleteReservation(id);
     }
 }
--- DotNetAPIControllers/Startup.cs
+++ DotNetAPIControllers/Startup.cs
@@ -1,23 +1,21 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using DotNetAPIControllers.Models;
-
 namespace DotNetAPIControllers
 {
     public class Startup
     {
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton < IRepository MemorRepository >;
+            services.AddSingleton<IRepository, MemoryRepository>();
             services.AddMvc();
         }
-
         public void Configure(IApplicationBuilder app)
         {
             app.UseStatusCodePages();
             app.UseDeveloperExceptionPage();
-
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
