049ba7b455827b1857842e3223b7826ef50f7a0f
Peter <peter.prentiss@gmail.com>
2018-04-22 14:31:56 -0500

    update client.js

--- DotNetAPIControllers/wwwroot/js/client.js
+++ DotNetAPIControllers/wwwroot/js/client.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function() {
+
+    $("form").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "api/reservation",
+            contentType: "application/json",
+            method: "POST",
+            data: JSON.stringify({
+                clientName: this.elements["ClientName"].value,
+                location: this.elements["Location"].value
+                }),
+            
+        })
+    })
+})
